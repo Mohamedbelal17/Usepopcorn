ecfd985ebd5c63532bfbab379167263a705af4e7
Mohamed Belal <111300150+Mohamedbelal17@users.noreply.github.com>
2024-08-15 21:55:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App-v2";
+import App from "./App-v2s";
 import StarRating from "./StarRating";
 
 // function Test() {
